28189bfe0d45c194ce3be83759747bf878e4a5eb
Alex Denisov <1101.debian@gmail.com>
2012-04-08 21:33:55 +0300

    BuildFramework target added

---
+++ BindableTypes/BindableTypes/BindableTypes.h
@@ -0,0 +1,10 @@
+//
+//  BindableTypes.h
+//  BindableTypes
+//
+//  Created by Alex Denisov on 08.04.12.
+//  Copyright (c) 2012 CoreInvader. All rights reserved.
+//
+
+#import "BTString.h"
+#import "BTNumber.h
